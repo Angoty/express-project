f1b6fccb384ff8817395dbda7571bba975873e52
Angoty <angotyrabarijaona@gmail.com>
2025-03-27 18:57:20 +0300

    changement test deploy7

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-// Route simple pour tester le déploiement
 app.get('/', (req, res) => {
     res.send('Bienvenue sur mon API déployée sur Vercel ! 🚀');
 });
 
-// Exporter l'application pour Vercel (PAS de `listen`)
 module.exports = app;
